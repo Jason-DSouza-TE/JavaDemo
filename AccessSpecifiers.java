2fddde8cb9f73416af424d6fbc49d64f1fe98869
Jason-DSouza-TE <rakeshjaisondsouza@gmail.com>
2022-03-14 11:51:07 +0530

    First Java File

---
+++ AccessSpecifiers.java
@@ -0,0 +1,23 @@
+package com.te.access.common;
+
+public class AccessSpecifiers {
+	public void publicMethod() {
+		System.out.println("this is public method");
+	}
+	protected void protectedMethod() {
+		System.out.println("this is protected method");
+	}
+	void defaultMethod() {
+		System.out.println("this is default method");
+	}
+	private void privateMethod() {
+		System.out.println("this is private method");
+	}
+public static void main(String[] args) {
+	AccessSpecifiers class1 = new AccessSpecifiers();
+	class1.defaultMethod();
+	class1.publicMethod();
+	class1.protectedMethod();
+	class1.privateMethod();
+}
+}
